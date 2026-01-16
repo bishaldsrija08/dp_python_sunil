580eaf3d6e9680926d8a0555b520d5170a5d751b
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-16 21:23:58 +0545

    first commit

---
+++ day1.py
@@ -0,0 +1,45 @@
+print("Bishal Rijal") # This line prints the name "Bishal Rijal" to the console
+
+""" "" => string => sequence of characters (alphanumeric value + special characters)
+
+I am bishal Rijal, bisharijal67, bishal@#rijal456
+Chitwan, Nepal => strinng
+abc45 => string
+
+Numeric value => mathmatically calculable value (+, -, *, /, %)
+ 45, -67
+
+Float value => decimal value => 56.7
+56, 78 => integer value
+
+"67" => string value
+67 => numeric value
+
+"6+"6" => "66" (concatenation)
+6+6 => 12 (addition)
+
+"6"+6 => Error (Type mismatch)"""
+
+
+print("6" + "6") # This line concatenates two string values "6" and "6" and prints "66" to the console
+print(6 + 6) # This line adds two numeric values 6 and 6 and prints 12 to the console
+print(6/6)
+print(6*6)
+print(6-6)
+
+# % => modulus operator => gives remainder value after division
+print(9 % 3)
+
+# // => floor division operator => gives quotient value after division [integer divider]
+print(7//2)
+
+# Python is case-sensitive language
+print("Bishal Rijal")
+
+print("Bishal" + " Rijal")
+
+""" => multi-line string
+I am Bishal Rijal, I am teaching python programming.
+I love programming.
+I love Nepal. I live in Chitwan. I love travelling.
+"""
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print("Bishal" + " Rijal")
