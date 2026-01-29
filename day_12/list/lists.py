ade0a3ee6047faa9774236deadc20fccbd11fbbb
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-29 08:48:42 +0545

    Add homework questions and examples for conditional statements and loops

---
+++ day_12/list/lists.py
@@ -0,0 +1,17 @@
+name1 = "Bishal"
+name2 = "Sunil"
+name3 = "Anil"
+name5 = "Ramesh"
+
+names = ["Bishal", "Sunil", "Anil", "Ramesh"]
+# print(names)
+
+for x in names:
+    print(x)
+
+# print(names[0])
+
+name = "Bishalu"
+print(name[0], 'hello im b')
+for x in name:
+    print(x)
